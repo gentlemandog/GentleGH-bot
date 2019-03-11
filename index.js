f0c2bd1f42d5bbf7947955d55563e9b473914683
clooooode <jackey8616@gmail.com>
2019-03-11 10:56:40 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var createHandler = require('github-webhook-handler')
+var http = require('http')
+
+var handler = createHandler({
+    path: '/webhook',
+    secret: '1234'
+})
+
+http.createServer((req, res) => {
+    handler(req, res, (err) => {
+        res.StatusCode = 404
+        res.end('No such location')
+    })
+}).listen(7777)
+
+handler.on('issues', function (event) {
+    console.log('Received an issue event for %s action=%s: #%d %s',
+      event.payload.repository.name,
+      event.payload.action,
+      event.payload.issue.number,
+      event.payload.issue.title)
+})
